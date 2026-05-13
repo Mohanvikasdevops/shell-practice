eb43bdf1875cef7fbda933351983e70fcd2db40c
Mohanvikas <vikas.gonugunta@gmail.com>
2026-05-13 18:39:51 +0530

    actions

--- disk-usage.sh
+++ disk-usage.sh
@@ -17,12 +17,12 @@ USAGE_THRESHOLD=3
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk '(print $6)' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk '{print $7}')
+    USAGE=$(echo $LINE | awk '(print $6)' | cut -d "%" -f1)
+    PARTITION=$(echo $LINE | awk '{print $7}')
 
     if [ "$USAGE" -ge  "$USAGE_THRESHOLD" ]; then
         MESSAGE+="High Disk usage on $PARTITION:$USAGE% \n"
     fi
 done <<< $DISK_USAGE
 
-echo -e "$MESSAGE"
+echo  "$MESSAGE"

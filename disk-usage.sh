ac09d1e2e9bf0a293a037251d5e38405248d4107
Mohanvikas <vikas.gonugunta@gmail.com>
2026-06-06 10:03:33 +0530

    actions

--- disk-usage.sh
+++ disk-usage.sh
@@ -5,7 +5,7 @@ G="\e[32m"
 y="\e[33m"
 N="\e[0m"
 MESSAGE=""
-
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 log(){
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
@@ -25,3 +25,5 @@ do
 done <<< "$DISK_USAGE"
 
 echo -e "$MESSAGE"
+
+sh mail.sh "info@joindevops.com" "High Disk Usage ALert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
---
+++ mail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+TO_ADDRESS=$1
+SUBJECT=$2
+MESSAGE_BODY=$3
+ALERT_TYPE=$4
+SERVER_IP=$5
+TO_TEAM=$6
+
+FINAL_MESSAGE=$(sed "s/TO_TEAM/$6/g" -e "s/ALERT_TYPE/$4/g" -e "s/SERVER_IP/$5/g" -e "s/MESSAGE/$3/g" template.html)
+
+{
+echo "To: $TO_ADDRESS"
+echo "Subject: $SUBJECT"
+echo "Content-Type: text/html"
+echo ""
+echo "$FINAL_MESSAGE"
+} | msmtp "$TO_ADDRESS"
